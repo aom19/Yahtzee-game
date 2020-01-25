bfa5aa8db40bc1dc9a443df7fbccb4eb49688c06
aom19 <39848284+aom19@users.noreply.github.com>
2020-01-25 21:02:07 +0200

    Yahtzee-game

---
+++ src/Die.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./Die.css";
+
+class Die extends Component {
+static defaultProps ={
+  numberWords : ["one" , "two" , "three" , "four" ,"five" , "six"]
+  , val : 5
+}
+  constructor(props){
+    
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+
+  }
+
+  handleClick(){
+    this.props.handleClick(this.props.idx);
+  }
+
+  render() {
+    
+     let classes = `Die fas fa-dice-${
+         this.props.numberWords[this.props.val - 1]
+      } fa-5x `;
+    if(this.props.locked) classes += 'Die-locked ';
+    if (this.props.rolling) classes += "Die-rolling"
+    return (
+      <i
+        className={classes}
+        onClick={ this.handleClick}
+        disabled = {this.props.disabled}
+      />
+      
+    );
+  }
+}
+
+export default Die;
